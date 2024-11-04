1d601380b7b8c4df140782e41b0712e742eb10e3
Marvi Fernando <syriefernando@gmail.com>
2024-11-05 00:23:48 +0800

    Initial commit

---
+++ userProfile.js
@@ -0,0 +1,8 @@
+let userProfile = {
+    fullName: "Daisyrie F. Fernando",
+    age: "19",
+    favoriteNumber: "16",
+    favoriteColors: ["Black", "Brown", "Grey"]
+};
+
+console.log(userProfile);
